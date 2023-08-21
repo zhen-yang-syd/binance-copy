620604f36a8f3c87d1931ddb98103ba2c8e98b52
Zhen Yang <webyangfullstack@gmail.com>
2023-08-21 17:03:17 +1000

    create leftnav1 section

--- components/ScrollBar.tsx
+++ components/ScrollBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ScrollBar = () => {
     return (
-        <div className='h-[215px] w-[6px] rounded-full bg-[#B7BDC6] absolute top-[2px] right-[2px]'></div>
+        <div className='h-[639px] w-[6px] rounded-full bg-[#B7BDC6] absolute top-[2px] right-[2px]'></div>
     )
 }
 
